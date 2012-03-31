cc317a0f9abd837f09bc7daed0a529c6a88bef9e
nicolasalderete <nicolasalderete@gmail.com>
2012-03-31 07:02:43 +0000

    Create project tecsat maven

---
+++ tecsat/tecsatServerMaven/src/main/java/ar/com/tecsat/server/TecsatServer.java
@@ -0,0 +1,19 @@
+package ar.com.tecsat.server;
+
+import java.util.Date;
+
+
+public class TecsatServer{
+	
+	/**
+	 * Tecsat Server
+	 * Author: Nicolas Alderete
+	 * Date: 11 de Marzo de 2012
+	 */
+	
+	public static void main(String[] args) {
+		
+	    System.out.println(ServerConstants.DateFormat.format(new Date()) + " Start: ");
+	    new ServidorUDP(4444);
+	}
+}
