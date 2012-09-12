69520d9800c07caa497828a76358c3cbe97c4668
nicolasalderete@gmail.com <nicolasalderete@gmail.com@1b06e26a-55c2-1df2-eb30-c46ca25009be>
2012-09-12 02:18:57 +0000

    web

---
+++ trunk/tecsatMaven/tecsat-web/src/main/java/ar/com/tecsat/view/VehiculoView.java
@@ -0,0 +1,53 @@
+/**
+ * @author Nicolas
+ * @version $Revision: $ $Date: $ $Author: $ $State: $
+ * 
+ */
+package ar.com.tecsat.view;
+
+import java.util.List;
+
+import javax.annotation.PostConstruct;
+import javax.ejb.EJB;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
+
+import ar.com.tecsat.modelo.Transporte;
+import ar.com.tecsat.service.local.VehiculoServiceLocal;
+
+/**
+ * @author Nicolas
+ *
+ */
+@ManagedBean(name="vehiculoView")
+@RequestScoped
+public class VehiculoView {
+
+	private List<Transporte> vehiculos;
+	
+	@EJB
+	private VehiculoServiceLocal vehiculoService;
+
+	public VehiculoView() {
+	}
+	
+	@PostConstruct
+	public void init() {
+		this.vehiculos = vehiculoService.transporteByCliente(1);
+	}
+	
+
+	/**
+	 * @return the vehiculos
+	 */
+	public List<Transporte> getVehiculos() {
+		return vehiculos;
+	}
+
+	/**
+	 * @param vehiculos the vehiculos to set
+	 */
+	public void setVehiculos(List<Transporte> vehiculos) {
+		this.vehiculos = vehiculos;
+	}
+}
